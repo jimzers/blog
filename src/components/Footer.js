d0761ade3d0b38fba8833ad11f4e40aad17c0a1b
jimzers <38634441+jimzers@users.noreply.github.com>
2020-06-06 13:28:21 -0700

    update products to my work in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,7 +37,7 @@ const Footer = class extends React.Component {
                     </li>
                     <li>
                       <Link className="navbar-item" to="/projects">
-                        Products
+                        My Work
                       </Link>
                     </li>
                     {/*<li>*/}
