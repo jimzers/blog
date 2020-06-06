224ea7d123df6c4783cc0c019594937eee72abdb
jimzers <38634441+jimzers@users.noreply.github.com>
2020-06-06 13:10:30 -0700

    temporarily update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
+import github from '../img/social/github.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -14,7 +15,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="jimzers"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
@@ -39,21 +40,21 @@ const Footer = class extends React.Component {
                         Products
                       </Link>
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
+                    {/*<li>*/}
+                    {/*  <Link className="navbar-item" to="/contact/examples">*/}
+                    {/*    Form Examples*/}
+                    {/*  </Link>*/}
+                    {/*</li>*/}
+                    {/*<li>*/}
+                    {/*  <a*/}
+                    {/*    className="navbar-item"*/}
+                    {/*    href="/admin/"*/}
+                    {/*    target="_blank"*/}
+                    {/*    rel="noopener noreferrer"*/}
+                    {/*  >*/}
+                    {/*    Admin*/}
+                    {/*  </a>*/}
+                    {/*</li>*/}
                   </ul>
                 </section>
               </div>
@@ -73,15 +74,23 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
               </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+              <div className="column is-2 social">
+                <a title="github" href="https://github.com/jimzers">
                   <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
+                      className="fas fa-lg"
+                      src={github}
+                      alt="Github"
+                      style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                {/*<a title="facebook" href="https://facebook.com">*/}
+                {/*  <img*/}
+                {/*    src={facebook}*/}
+                {/*    alt="Facebook"*/}
+                {/*    style={{ width: '1em', height: '1em' }}*/}
+                {/*  />*/}
+                {/*</a>*/}
+                <a title="twitter" href="https://twitter.com/jimzersML">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -89,20 +98,20 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
+                {/*<a title="instagram" href="https://instagram.com">*/}
+                {/*  <img*/}
+                {/*    src={instagram}*/}
+                {/*    alt="Instagram"*/}
+                {/*    style={{ width: '1em', height: '1em' }}*/}
+                {/*  />*/}
+                {/*</a>*/}
+                {/*<a title="vimeo" href="https://vimeo.com">*/}
+                {/*  <img*/}
+                {/*    src={vimeo}*/}
+                {/*    alt="Vimeo"*/}
+                {/*    style={{ width: '1em', height: '1em' }}*/}
+                {/*  />*/}
+                {/*</a>*/}
               </div>
             </div>
           </div>
